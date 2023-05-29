417a374bf2179bd6225b1d1d23d35ffb39044422
benjamin.fuchs <benjamin.fuchs@valtech.com>
2023-05-29 10:35:19 +0200

    Implement boolean retrieval

--- QueryProcessor.py
+++ QueryProcessor.py
@@ -1,4 +1,6 @@
+import copy
 import random
+import sys
 from Tokenizer import *
 from Merger import *
 
@@ -66,6 +68,7 @@ class QueryProcessor:
 
     # Verarbeitet eine boolesche Anfrage in Normalform und gibt die entsprechenden Suchergebnisse zurück
     # (NOT "term1 term2" OR NOT term3 \4 term5) AND NOT term4 \3 term5 AND "term1 term3 term4" AND term4
+    # (NOT the OR NOT a \20 the OR a b c OR d) AND NOT read write AND (the OR a) AND the
     def process_query(self, query):
         query_tokens = self.tokenizer.tokenizeQuery(query)
         #results = set(self._get_posting_ids(query_tokens[0]))
@@ -78,32 +81,77 @@ class QueryProcessor:
                 filtered_part = list(filter(r.match, query_part[i]))
                 if type(query_part[i]) is list and len(filtered_part) > 0:
                     result = self.merger.positional_intersect(query_part[i][0], query_part[i][2], 3)
-                    id = "#&" + str(random.random())
-                    index_lists[id] = result
-                    query_part[i] = id
+                    result_id = "#&" + str(random.random())
+                    index_lists[result_id] = result
+                    query_part[i] = result_id
                 elif type(query_part[i]) is list and len(query_part[i]) > 1:
                     result = self.merger.phrase_query(query_part[i])
-                    id = "#&" + str(random.random())
-                    index_lists[id] = result
-                    query_part[i] = id
+                    result_id = "#&" + str(random.random())
+                    index_lists[result_id] = result
+                    query_part[i] = result_id
 
-        for query_part in query_tokens:
+        for i in range(len(query_tokens)):
+            query_part = query_tokens[i]
             while "NOT" in query_part:
-                not_pos = query_part.indx("NOT")
-                if type(query_part[not_pos + 1]) is list:
-                    position_list = self.index.get_document_list(query_part[not_pos + 1][0])
+                or_pos = query_part.index("NOT")
+                if type(query_part[or_pos + 1]) is list:
+                    position_list = self.index.get_document_list(query_part[or_pos + 1][0])
                 else:
-                    position_list = index_lists[query_part[not_pos + 1]]
+                    position_list = index_lists[query_part[or_pos + 1]]
                 result = self.merger.not_merge(position_list, self.document_count)
-                id = "#&" + str(random.random())
-                index_lists[id] = result
-                query_part[i] = id #anstatt query_part in der for Schleife muss durch query_tokens mit einem integer iteriert werden
+                result_id = "#&" + str(random.random())
+                index_lists[result_id] = result
+                query_tokens[i][or_pos] = result_id
+                query_tokens[i].pop(or_pos + 1)
 
+            while "OR" in query_part:
+                or_pos = query_part.index("OR")
+                first_operand = query_part[or_pos - 1]
+                second_operand = query_part[or_pos + 1]
+                if type(first_operand) is list:
+                    position_list_first_operand = self.index.get_document_list(first_operand[0])
+                else:
+                    position_list_first_operand = index_lists[first_operand]
+                if type(second_operand) is list:
+                    position_list_second_operand = self.index.get_document_list(second_operand[0])
+                else:
+                    position_list_second_operand = index_lists[second_operand]
+                result = self.merger.or_merge(position_list_first_operand, position_list_second_operand)
+                result_id = "#&" + str(random.random())
+                index_lists[result_id] = result
+                query_tokens[i][or_pos] = result_id
+                query_tokens[i].pop(or_pos + 1)
+                query_tokens[i].pop(or_pos - 1)
 
+            if not re.match(r'(#&).*', query_part[0]):
+                result = self.index.get_document_list(query_part[0])
+                result_id = "#&" + str(random.random())
+                index_lists[result_id] = result
+                query_tokens[i][0] = result_id
 
-        print("test")
+        sorted_query = []
+        copied_query_tokens = copy.deepcopy(query_tokens)
+        while len(copied_query_tokens) > 0:
+            shortest = sys.maxsize
+            shortest_index = 0
+            for i in range(len(copied_query_tokens)):
+                token = copied_query_tokens[i][0]
+                if len(index_lists[token]) < shortest:
+                    if len(index_lists[token]) == 0:
+                        return []  # If one of the index_lists is [] than we can instantly return [].
+                    shortest = len(index_lists[token])
+                    shortest_index = i
+            sorted_query.append(copied_query_tokens[shortest_index][0])
+            copied_query_tokens.pop(shortest_index)
 
-        #return results
+        while len(sorted_query) != 1:
+            result = self.merger.and_merge_fast(index_lists[sorted_query[0]], index_lists[sorted_query[1]])
+            result_id = "#&" + str(random.random())
+            index_lists[result_id] = result
+            sorted_query[0] = result_id
+            sorted_query.pop(1)
+
+        return index_lists[sorted_query[0]]
 
     # Parst die boolesche Anfrage in Normalform und gibt die Liste der Tokens zurück
     def _parse_query(self, query):
